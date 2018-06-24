61ef13366e1396c2b0a3fcd341bdb9acb70bf1f3
DenisGladkiy <gladkiy@gmail.com>
2018-06-24 23:29:34 +0300

    Lesson 1.7, combinedEventLogger ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,26 +12,34 @@ import java.util.Map;
  */
 public class App {
     private Client client;
-    private loggers.EventLogger EventLogger;
+    private EventLogger defaultEventLogger;
+    private Map<EventType,EventLogger> eventLoggers;
 
-    public App(Client client, Map<EventType,EventLogger> EventLoggers) {
+    public App(Client client, EventLogger defaultEventLogger, Map<EventType,EventLogger> eventLoggers) {
         this.client = client;
-        this.EventLogger = EventLogger;
+        this.defaultEventLogger = defaultEventLogger;
+        this.eventLoggers = eventLoggers;
     }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         App app = (App)context.getBean("app");
         Event event = (Event)context.getBean("event");
-        for(int i = 0; i < 12; i++)
-        app.logEvent(event);
-        //app.logEvent("entity.Event for user 2");
+        for(int i = 0; i < 3; i++) {
+            app.logEvent(null, event);
+        }
+        app.logEvent(EventType.INFO, event);
+        app.logEvent(EventType.ERROR, event);
         context.close();
     }
 
-    public void logEvent(Event event){
+    public void logEvent(EventType type, Event event){
         String message = event.getMsg().replaceAll(client.getId(), client.getFullName());
         event.setMsg(message);
-        EventLogger.logEvent(event);
+        EventLogger logger = eventLoggers.get(type);
+        if(logger == null){
+            logger = defaultEventLogger;
+        }
+        logger.logEvent(event);
     }
 }
